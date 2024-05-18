b6730f7de1ad87ee565a49e064a01760c29b4cee
Alex Eremie <alex.eremie19@gmail.com>
2024-05-18 10:43:18 +0200

    added firebase to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any>;
   title = 'ringoffire';
+
+  constructor(){
+    const aCollection = collection(this.firestore, 'items');
+    this.items$ = collectionData(aCollection);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"ring-of-fire-2ae12","appId":"1:578535188697:web:55e486266bc025d465f6c8","storageBucket":"ring-of-fire-2ae12.appspot.com","apiKey":"AIzaSyA3f1g1O51gOeSYEme5utLMp7DyJOGgV4Q","authDomain":"ring-of-fire-2ae12.firebaseapp.com","messagingSenderId":"578535188697"})), provideFirestore(() => getFirestore())]
 };
